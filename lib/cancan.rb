f2323b7c585898f5d161ff54e3be3b65b5391fd4
kshet26 <keith_shetler@hotmail.com>
2010-02-26 19:42:49 -0700

    Added instance variables for redirect

--- lib/cancan.rb
+++ lib/cancan.rb
@@ -1,7 +1,16 @@
 module CanCan
   # This error is raised when a user isn't allowed to access a given
-  # controller action. See ControllerAdditions#unauthorized! for details.
-  class AccessDenied < StandardError; end
+  # controller action. The method variables are provided so devs
+  # can perform a recovery in the app's controller. See
+  # ControllerAdditions#unauthorized! for details.
+  #
+  class AccessDenied < StandardError
+    attr_reader :denied_action, :denied_noun
+    def initialize(denied_action, denied_noun)
+      @denied_action = denied_action
+      @denied_noun = denied_noun
+    end
+  end
 end
 
 require File.dirname(__FILE__) + '/cancan/ability'
